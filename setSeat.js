b9ab3638673b5d2f1e87ec92baac03e985f33024
pestrinmarco <pestrinmarco@gmail.com>
2020-01-26 12:31:06 +0100

    third client

--- setSeat.js
+++ setSeat.js
@@ -5,7 +5,8 @@ var priceFirst = 50, priceSecond = 40, priceThird = 25;
 var seatToLocate = 1;
 var id = 0;
 createMatrix();
-seatToLocate == 1 ? assegnateSeatOnePerson() : null
+
+assegnateSeatTwoPerson()
 seatToLocate == 1 ? assegnateSeatOnePerson() : null
 
 function changeInfo ( value, desc ) {
@@ -36,8 +37,9 @@ function createMatrix() {
                 price=priceThird
             }
             var seat = {
-                "row":positionSeat,
-                "column":i,
+                "note": "",
+                "column":positionSeat,
+                "row":i,
                 "price":price,
                 "id":id,
                 "occuped":false
@@ -50,14 +52,17 @@ function createMatrix() {
 
 function assegnateSeatOnePerson(){
     var firstCycle = true;
+    var terzaClasseUsufruita = false
     matrix.map((item,index) => {
         var res = null
         if(item.price == priceThird){
-            if(item.row != "B" && item.row != "E" && !item.occuped){
+            if(item.column != "B" && item.column != "E" && !item.occuped){
+                terzaClasseUsufruita = true
                 if(firstCycle){
                     res = matrix.filter((value) => {
                         if(value.id === index) {
                             value.occuped = true
+                            value.note = " singolo" + index
                             firstCycle = false;
                             return value
                         }else{
@@ -69,5 +74,89 @@ function assegnateSeatOnePerson(){
             }
         }
     })
+    if(terzaClasseUsufruita){
+        firstCycle = true
+        matrix.map((item,index) => {
+            var res = null
+            if(item.price == priceSecond){
+                if(item.column != "B" && item.column != "E" && !item.occuped){
+                    if(firstCycle){
+                        console.log("entrato qui");
+                        res = matrix.filter((value) => {
+                            if(value.id === index) {
+                                value.occuped = true
+                                value.note = " singolo"
+                                firstCycle = false;
+                                return value
+                            }else{
+                                return null
+                            }
+                        })
+                        changeInfo(index,matrix)
+                    }
+                }
+            }
+        })
+    }
+}
+
+function assegnateSeatTwoPerson(){
+    var firstCycle = true;
+    matrix.map((item,index) => {
+        var res = null
+        if(item.price == priceThird){
+            // i due posti devono essere vicini e non già occupati
+            // il posto non deve essere in colonna C e F
+            if(!item.occuped && !matrix[index+1].occuped && item.column != "C" && item.column != "F"){
+                if(firstCycle){
+                    res = matrix.filter((value) => {
+                        if(value.id === index) {
+                            matrix[index+1].occuped = true;
+                            value.occuped = true;
+                            matrix[index+1].note = "coppia "+index;
+                            value.note = "coppia "+ index;
+                            firstCycle = false;
+                            return value
+                        }else{
+                            return null
+                        }
+                    })
+                    changeInfo(index,matrix)
+                }
+            }
+        }
+    })
+}
+function assegnateSeatThreePerson(){
+    var firstCycle = true;
+    matrix.map((item,index) => {
+        var res = null
+        if(item.price == priceThird){
+            //la riga di posti deve essere metà libera
+            if(
+                !item.occuped &&
+                (item.column == "A" || item.column == "D") &&
+                !matrix[index+1].occuped && !matrix[index+2].occuped
+            ){
+                if(firstCycle){
+                    res = matrix.filter((value) => {
+                        if(value.id === index) {
+                            matrix[index+1].occuped = true;
+                            matrix[index+2].occuped = true;
+                            value.occuped = true;
+                            matrix[index+1].note = "tris "+index;
+                            matrix[index+2].note = "tris "+index;
+                            value.note = "tris "+ index;
+                            firstCycle = false;
+                            return value
+                        }else{
+                            return null
+                        }
+                    })
+                }
+            }
+        }
+    })
     console.log(matrix)
+
 }
